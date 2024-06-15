4e86234d2f49aa06de41c6da52e768484dfc8e81
Loochnick <loochnik@ukr.net>
2024-06-15 20:16:29 +0200

    Homepage url

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
